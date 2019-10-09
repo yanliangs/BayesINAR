aa194325feef2098fb17eca9df515fc40c55f7be
heltongraziadei <hltgraziadei@gmail.com>
2019-10-09 15:33:57 -0300

    ...

--- R/Pittsburgh.R
+++ R/Pittsburgh.R
@@ -2,6 +2,6 @@
 #'
 #' A dataset containing the burglary reported cases in Pitttsburgh, USA, from January 1990 to December 2001.
 #'
-#' @format A data frame with 144 months and 36 patrol areas:
+#' @format A data frame with 144 months and 36 patrol areas.
 #' @source \url{http://www.forecastingprinciples.com/index.php/crimedata}
 "Pittsburgh"
